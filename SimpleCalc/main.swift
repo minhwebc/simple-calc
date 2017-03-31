23fc39051551c808e6567a91164d6741c7538eba
Quan Nguyen <quannguyen@Tonys-MacBook-Air.local>
2017-03-31 12:49:33 -0700

    Initial Commit

---
+++ SimpleCalc/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SimpleCalc
+//
+//  Created by Quan Nguyen on 3/31/17.
+//  Copyright © 2017 Quan Nguyen. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
